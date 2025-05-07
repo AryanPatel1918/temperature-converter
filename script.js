b8dc4e34e9e133897a86197b6dbf17ce17fe0142
aryanpatel1918 <aryanpatel1918@gmail.com>
2025-05-07 15:16:46 -0400

    Improve input handling and validation logic

--- script.js
+++ script.js
@@ -8,16 +8,39 @@ convertBtn.addEventListener("click", convert)
 
 function convert() {
 
+    displayTemp.style.display = "block"
+    
+    if (!tempInput.value) {
+        displayTemp.style.color = "red"
+        displayTemp.textContent = "Please enter a temperature."
+        return
+    }
+
     const inputValue = parseFloat(tempInput.value)
 
     if (ftocRadio.checked) {
         const celsius = ((inputValue - 32) * 5/9).toFixed(2)
-        displayTemp.textContent = `${celsius} °C`;
+        displayTemp.style.color = "black";
+        displayTemp.textContent = `${celsius}°C`;
     } else if (ctofRadio.checked) {
         const fahrenheit = ((inputValue * 9/5) + 32).toFixed(2)
-        displayTemp.textContent = `${fahrenheit} °F`
+        displayTemp.style.color = "black";
+        displayTemp.textContent = `${fahrenheit}°F`
     } else {
+        displayTemp.style.color = "red"
         displayTemp.textContent = "Please select a conversion type."
     }
 }
 
+// Hide display message when input or radio changes
+tempInput.addEventListener("input", () => {
+    displayTemp.style.display= "none"
+})
+
+ftocRadio.addEventListener("change", () => {
+    displayTemp.style.display = "none"
+})
+
+ctofRadio.addEventListener("change", () => {
+    displayTemp.style.display = "none"
+})
